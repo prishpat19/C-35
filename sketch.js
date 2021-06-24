4fa5d32cb51cfe4d931766b237dc2256a6327cf2
prishpat19 <72462997+prishpat19@users.noreply.github.com>
2021-06-24 18:49:25 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+var hello;
+var database, position;
+
+function setup(){
+    database = firebase.database();
+
+    createCanvas(500,500);
+
+    hello = createSprite(250,250,10,10);
+    hello.shapeColor = "red";
+
+    var helloPosition = database.ref("ball/position");
+    helloPosition.on("value", readPosition, showError);
+}
+
+function draw(){
+    background("white");
+    if(position !== undefined){
+
+   
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+
+function writePosition(x,y){
+   database.ref("ball/position").set({
+
+    'x': position.x + x, 
+    'y': position.y + y,    
+   })
+}
+
+function readPosition(data){
+
+    position = data.val();
+    hello.x = position.x;
+    hello.y = position.y;
+
+}
+
+function showError(){
+    console.log("code is not working")
+}
+
+
